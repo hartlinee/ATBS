968b3d7b4d25d809d5af0ccce8784bf8aa57d69b
Evey <66142501+hartlinee@users.noreply.github.com>
2021-04-12 11:28:50 -0400

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#hello world python
+
+print('Hello world!')
+print('What is your name?')
+
+name = input()
+
+print('Hello, ' + name + '!')
+print('The length of your name is ')
+print(len(name))
+
+print('What is your age?')
+age = input()
+print('You will be ' + str(int(age) + 1) + ' in a year') 
